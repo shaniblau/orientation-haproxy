81144de8894a23948d710ad49a02e35bda6da47a
שני בלאו <shaniblau@snys-MacBook-Air.local>
2023-06-04 19:05:06 +0300

    try

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ def read_root():
 
 
 @app.post("/uploadfile")
-async def create_upload_file(file: UploadFile):
-    name = file.filename
-    if not os.path.exists("../dogs"):
-        os.mkdir("../dogs")
-    file_path = os.path.join("../dogs", name)
-    with open(file_path, 'wb') as f:
-        f.write(await file.read())
-    return {"filename": name}
+async def create_upload_file(files: List[UploadFile]):
+    saved_files = []
+    for file in files:
+        name = file.filename
+        if not os.path.exists("../dogs"):
+            os.mkdir("../dogs")
+        file_path = os.path.join("../dogs", name)
+        with open(file_path, 'wb') as f:
+            f.write(await file.read())
+        saved_files.append(name)
+    return {"filenames": saved_files}
