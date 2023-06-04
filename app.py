e62111b7c03587fe5c0cda19298e5225662dd880
שני בלאו <shaniblau@snys-MacBook-Air.local>
2023-06-04 18:29:23 +0300

    try

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
@@ -8,6 +10,11 @@ def read_root():
     return {"name": "shani blau"}
 
 
-@app.post("/uploadfile/")
+@app.post("/uploadfile")
 async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+    name = file.filename
+    if not os.path.exists("../dogs"):
+        os.mkdir("../dogs")
+    with open(f"../dogs/{name}", 'wb') as file:
+        file.read()
+    return {"filename": name}
